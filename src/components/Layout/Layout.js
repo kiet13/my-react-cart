830fb79c8d7df5d7e36cbd072e5c1a2e813488ff
Nguyen Thanh Kiet <kiet13.itus@gmail.com>
2021-10-18 14:04:43 +0700

    first commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Layout.module.scss';
+import Header from '../Header/Header';
+
+export default function Layout() {
+  return (
+    <>
+      <Header />   
+    </>
+  )
+}
